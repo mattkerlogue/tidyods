f395e7a3778e0cf4cf3598305c35ecae86759ae3
Matt Kerlogue <matt.kerlogue@me.com>
2023-04-27 00:19:17 +0100

    correct rectifier

--- R/rectify.R
+++ R/rectify.R
@@ -148,7 +148,7 @@ quick_rectify <- function(ods_cells) {
     dplyr::left_join(ods_cells, by = c("row", "col")) |>
     dplyr::mutate(col = paste0("x", col)) |>
     dplyr::select(row, col, base_value) |>
-    tidyr::pivot_wider(names_from = row, values_from = base_value)
+    tidyr::pivot_wider(names_from = col, values_from = base_value)
 
 }
 
